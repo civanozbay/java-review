630cc3e24d198eadf48aa08e1b5ca38076bf21df
civanozbay <civan417@hotmail.com>
2022-08-07 20:22:55 +0200

    lombok added

---
+++ lambda/src/main/java/Student.java
@@ -0,0 +1,19 @@
+import lombok.*;
+
+//@NoArgsConstructor
+@AllArgsConstructor
+@Data
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+public class Student {
+    private String firstName;
+    private String lastName ;
+    private String userName ;
+    private String email ;
+    private String password ;
+    private String confirmPassword ;
+    private int age;
+
+}
